44d06d3f78b908bbe29a91132622b5f433b9ef2c
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-01-29 21:27:12 +0600

    Added User Name at Login Page

--- login.js
+++ login.js
@@ -1,5 +1,16 @@
+let User_name = JSON.parse(localStorage.getItem("Name")) || "";
+
+// Ask for the user's name if not already set
+while (!User_name) {
+    User_name = prompt("Enter Your Name");
+    if (User_name) {
+        localStorage.setItem("Name", JSON.stringify(User_name));
+    }
+}
+
 let pass = JSON.parse(localStorage.getItem("pass")) || "";
-// take password form user first time
+
+// Ask for password if not already set
 while (!pass || pass.length < 4) {
     pass = prompt("Create a password for your next login. Please note, if you forget this password, you will not be able to recover it and will lose all your data. The password must be at least 4 digits.");
     if (pass && pass.length >= 4) {
@@ -9,6 +20,7 @@ while (!pass || pass.length < 4) {
 
 
 pass = JSON.parse(localStorage.getItem("pass")) || "";
+
 
 
 // Check password for log in
